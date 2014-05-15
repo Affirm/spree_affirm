3a3e78ab07de9f4a8f35cb6635d43f45cc8d7e62
Brett Wejrowski <brett@wejrowski.com>
2014-05-14 18:26:33 -0700

    updated migration

--- db/migrate/20140514194315_create_affirm_checkout.rb
+++ db/migrate/20140514194315_create_affirm_checkout.rb
@@ -1,6 +1,6 @@
 class CreateAffirmCheckout < ActiveRecord::Migration
   def change
-    create_table :affirm_checkouts do |t|
+    create_table :spree_affirm_checkouts do |t|
       t.string     :token
       t.timestamps
     end
