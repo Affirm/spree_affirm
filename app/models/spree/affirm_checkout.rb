534b064c5a4009cb01c7b4bfea40a82cb1e948ad
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2014-07-02 21:19:55 -0700

    [fix] remove 'try'

--- app/models/spree/affirm_checkout.rb
+++ app/models/spree/affirm_checkout.rb
@@ -112,7 +112,7 @@ module Spree
         street1:      spree_address.address1,
         street2:      spree_address.address2,
         postal_code:  spree_address.zipcode,
-        region1_code: spree_address.state.try(:abbr)
+        region1_code: spree_address.state.abbr
 
       # check that each value from affirm matches the spree address
       }.each do |affirm_key, spree_val|
